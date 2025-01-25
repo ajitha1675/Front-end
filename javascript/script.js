44471be91085a98f71b18a0ad0012a9fbb9168a8
Ajilalitha <ajilalitha010@gmail.com>
2025-01-25 12:05:26 +0530

    var,let,const difference

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,57 @@
 let uName = "Dark";
+console.log(uName);
+
+let fName = "Dark";
+let lName = "Knight";
+
+console.log(fName, lName);
+
+let $kName = "single page application";
+let _oName = "spa";
+// let 0name = "hji"; //numeric value is not allowed as a first letter
+
+
+console.log($kName,_oName);
+//camel case
+//pascale case
+//under case
+
+let newemployeeid = 10;
+
+let newemployeeId = 10 //camel case
+let NewEmployeeId = 10 //pascal case
+let New_Employee_Id = 10 //under case
+
+var age = 10; //declaration and initialization
+
+var num; //declaration
+num = 40; //initialization or assiging
+
+var age = 80; //redeclaration
+
+
+// console.log(age);
+
+let newAge = 30;//re-declare is not possible in let
+
+console.log(newAge);
+
+const studentName = "John";
+
+//  studentName ="abnc";
+
+//  console.log(studentName);
+
+//printing statement
+// document.write(newAge) //it will print the value on the browser
+// confirm(newAge)
+
+// let studentAge = prompt("Enter your age"); //promt used to the dynamic value from the user
+
+// console.log(studentAge);
+let studentAge;
+console.error(studentAge);
+console.warn(studentAge);
+
+console.clear();
+
