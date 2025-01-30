4808307e92a0fee1d127be5e5fdf3c4603bc971a
Ajilalitha <ajilalitha010@gmail.com>
2025-01-30 13:22:59 +0530

    data types

--- javascript/script.js
+++ javascript/script.js
@@ -49,9 +49,64 @@ const studentName = "John";
 // let studentAge = prompt("Enter your age"); //promt used to the dynamic value from the user
 
 // console.log(studentAge);
-let studentAge;
-console.error(studentAge);
-console.warn(studentAge);
+// let studentAge;
+// console.error(studentAge);
+// console.warn(studentAge);
 
-console.clear();
+// console.clear();
+
+//primitive d/y
+ var num = 120;
+ var num = 1.25;
+
+//string
+
+let username = "Javescript is a scripting language";
+
+username = 'js is a single threaded language';
+username = `ECMA script`;
+
+//boolean
+let condition = true;
+
+//undefined
+let noValue;
+
+//null
+let emptyValue = null;
+
+
+console.log(num, username, condition, noValue, emptyValue);
+
+//single line comment
+
+/*
+multi line comment
+js
+react js
+*/
+
+//non-primitive data type or reference type
+
+//array 
+let multipleUsers = ["React.js", "JS","Insta","FB"];
+console.log(multipleUsers);
+
+console.log(multipleUsers[0]);
+
+//object -- key value pair
+let vehicle = { 
+    vehicleType: "Four wheeler",
+    brand: "Honda",
+    price: "10L",
+    fueltype: "Petrol"
+}
+
+console.log(vehicle);
+console.log(vehicle.vehicleType);
+console.log(vehicle.price);
+
+
+
+ 
 
